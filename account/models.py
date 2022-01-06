832ccd15c973a6b46129d74e2a8367549fa3e5d8
E4Elit3 <94747023+E4Elit3@users.noreply.github.com>
2022-01-06 17:43:53 +0530

    aa

--- account/models.py
+++ account/models.py
@@ -32,7 +32,10 @@ class MyAccountManager(UserManager):
 
 class Account(AbstractUser):
 	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
-	username 				= models.CharField(max_length=30, unique=True)
+	fname 					= models.CharField(max_length=30)
+	lname 					= models.CharField(max_length=30)
+	phone 					= models.CharField(max_length=20)
+	address					= models.CharField(max_length=128)
 	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
 	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
 	is_admin				= models.BooleanField(default=False)
--- app/views.py
+++ app/views.py
@@ -1,37 +1,53 @@
 from django.http.response import HttpResponse
-from django.shortcuts import  render
+from django.shortcuts import  redirect, render
+
+from account.models import Account
 from .models import Coin, UserPortfolio
 
 def home(request):
     return render(request,'index.html')
 
 def userprofile(request):
-    return render(request,'user/userprofile.html')
+    if request.method == 'POST':
+        obj= Account.objects.get(id=request.user.id)
+        obj.username=request.POST['fname']
+        obj.lname=request.POST['lname']
+        obj.phone=request.POST['pnum']
+        obj.address=request.POST['address']
+        obj.save()
+    list=Account.objects.all()
+    return render(request,'user/userprofile.html',{'list':list})
 
 def userdashboard(request):
-    list=UserPortfolio.objects.get(user=request.user)
-    return render(request,'user/userdashboard.html',{'list':list})
+    list=UserPortfolio.objects.filter(user=request.user)
+    alist=Coin.objects.last()
+    return render(request,'user/userdashboard.html',{'list':list,'alist': alist})
 
 def adminbase(request):
     return render(request,'admin/admin.html')    
 
 def admindashboard(request):
-    return render(request,'admin/admindashboard.html')     
+    
+    return render(request,'admin/admindashboard.html')
 
 def adminprofile(request):
     return render(request,'admin/adminprofile.html')        
 
 def adminctsdetails(request):
-    return render(request,'admin/adminctsdetails.html')        
+
+    data = Account.objects.all() 
+
+    return render(request,'admin/adminctsdetails.html',{"users":data})        
 
 def adminNAV(request):
-    return render(request,'admin/adminNAV.html')        
+    list=Coin.objects.last()
+    return render(request,'admin/adminNAV.html',{'list':list})             
 
 def coin(request):
     if request.method=='POST':
         obj = Coin()
         obj.nav = request.POST['nav']
         obj.save()
-        return HttpResponse('Successful')
+        return redirect('app:adminNAV')
     return HttpResponse('/')
     
