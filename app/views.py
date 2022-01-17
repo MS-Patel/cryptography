c6a40c43fdd1c67b441bebd1e2622dbd9fce6607
E4Elit3 <94747023+E4Elit3@users.noreply.github.com>
2022-01-17 17:54:59 +0530

    zzz

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def home(request):
 
 def userprofile(request):
     if request.method == 'POST':
-        obj= Account.objects.get(id=request.user.id)
+        obj=Account.objects.get(id=request.user.id)
         obj.username=request.POST['fname']
         obj.lname=request.POST['lname']
         obj.phone=request.POST['pnum']
@@ -31,7 +31,19 @@ def admindashboard(request):
     return render(request,'admin/admindashboard.html')
 
 def adminprofile(request):
-    return render(request,'admin/adminprofile.html')        
+    if request.method == 'POST':
+        obj= Account.objects.get(id=request.user.id)
+        obj.username=request.POST['fname']
+        obj.email=request.POST['email']
+        obj.date_joined=request.POST['date_joined']
+        obj.last_login=request.POST['last_login']
+        obj.lname=request.POST['lname']
+        obj.phone=request.POST['pnum']
+        obj.address=request.POST['address']
+        obj.save()
+    list=Account.objects.all()
+    port=UserPortfolio.objects.all()
+    return render(request,'admin/adminprofile.html',{'list':list,"port":port})        
 
 def adminctsdetails(request):
 
